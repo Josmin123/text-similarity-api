fd105eb1e80240096adf37ed89d0037000f8eae5
josmin <josmintheresa98@gmail.com>
2025-04-23 15:01:03 +0530

    Fixed score issue

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from model import load_model
 from similarity import compute_similarity
 
 model=load_model()
-text1="Tsunami hit the coast of japan"
-text2="Earthquake caused massive waves in japan"
+text1 = "Apples are fruits."
+text2 = "my name is josmin"
+
 
 score=compute_similarity(text1,text2,model)
 print(f"similarity score is :{score}")
--- similarity.py
+++ similarity.py
@@ -1,6 +1,10 @@
 from sklearn.metrics.pairwise import cosine_similarity
 
-def compute_similarity(text1,text2,model):
-    emdeddings=model.encode([text1,text2])
-    score=cosine_similarity([emdeddings[0],emdeddings[1]])[0][0]
-    return round(float(score),2)
+from sentence_transformers import util
+
+def compute_similarity(text1, text2, model):
+    embedding1 = model.encode(text1, convert_to_tensor=True)
+    embedding2 = model.encode(text2, convert_to_tensor=True)
+
+    score = util.pytorch_cos_sim(embedding1, embedding2)
+    return round(score.item(), 2)
